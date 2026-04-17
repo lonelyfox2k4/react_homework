87fa9047f062b6aa04c7f2acb88115455824f239
Le Trg Son <truongson19062004@gmail.com>
2026-04-17 15:08:10 +0700

    up conditional rendering

---
+++ react/src/App.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+export default function UserList() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    setTimeout(() => {
+   
+      const success = true;
+
+      if (success) {
+        setUsers([
+          { id: 1, name: "Sơn" },
+          { id: 2, name: "Nam" }
+        ]);
+      } else {
+        setError("Fetch failed");
+      }
+
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      {users.length > 0 && <p>Có {users.length} user</p>}
+      {users.length === 0 ? (
+        <p>Không có user nào</p>
+      ) : (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
